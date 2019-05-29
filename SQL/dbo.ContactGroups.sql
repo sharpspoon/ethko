95588c549ff3c479a8501f537114893fdacff84e
Robin Ward <robin@steelcitysites.net>
2019-05-29 15:04:58 -0500

    cleaned up SQL files

--- SQL/dbo.ContactGroups.sql
+++ SQL/dbo.ContactGroups.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[ContactGroups] (
     [ContactGroupId]   INT            IDENTITY (1, 1) NOT NULL,
     [ContactGroupName] NVARCHAR (50)  NOT NULL,
-    [InsDate]          DATETIME2        NOT NULL,
+    [InsDate]          DATETIME2 (7)  NOT NULL,
     [FstUser]          NVARCHAR (128) NOT NULL,
     PRIMARY KEY CLUSTERED ([ContactGroupId] ASC),
     CONSTRAINT [FK_ContactGroups_AspNetUsers] FOREIGN KEY ([FstUser]) REFERENCES [dbo].[AspNetUsers] ([Id])
