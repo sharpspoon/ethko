69e448990b31e973792eadc5e8510332aa1f2e2e
Robin Ward <robin@steelcitysites.net>
2019-06-11 16:38:36 -0500

    edit contact work

--- ethko/Models/ContactViewModels.cs
+++ ethko/Models/ContactViewModels.cs
@@ -134,13 +134,89 @@ namespace ethko.Models
         public ContactGroup contactGroup { get; set; }
     }
 
-    public class ContactIndividualViewModel
+    public class GetIndividualContactViewModel
     {
-        [Display(Name = "Name")]
-        public string Name { get; set; }
+        [Display(Name = "ContactId")]
+        public int ContactId { get; set; }
+
+        [Display(Name = "CompanyId")]
+        public int CompanyId { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FName { get; set; }
+
+        [Display(Name = "Middle Name")]
+        public string MName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LName { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Client Portal")]
+        public string EnableClientPortal { get; set; }
+
+        [Display(Name = "Contact Group")]
+        public string ContactGroupId { get; set; }
+
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+
+        [Display(Name = "Work Phone")]
+        public string WorkPhone { get; set; }
+
+        [Display(Name = "Home Phone")]
+        public string HomePhone { get; set; }
+
+        [Display(Name = "Fax")]
+        public string Fax { get; set; }
+
+        [Display(Name = "Job Title")]
+        public string JobTitle { get; set; }
+
+        [Display(Name = "Birthday")]
+        public string Birthday { get; set; }
+
+        [Display(Name = "License")]
+        public string License { get; set; }
+
+        [Display(Name = "Website")]
+        public string Website { get; set; }
+
+        [Display(Name = "Notes")]
+        public string Notes { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Display(Name = "Address2")]
+        public string Address2 { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "State")]
+        public string State { get; set; }
+
+        [Display(Name = "Zip")]
+        public string Zip { get; set; }
+
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+        [Display(Name = "Archived")]
+        public int Archived { get; set; }
+
+        [Display(Name = "UserId")]
+        public string UserId { get; set; }
+
+        [Display(Name = "InsDate")]
+        public string InsDate { get; set; }
     }
 
 
-    
+
 
 }
